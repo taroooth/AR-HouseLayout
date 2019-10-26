a7b371cb380a5901e13634eb5a1ba7b8e7ae03a3
taroooth <52551214+taroooth@users.noreply.github.com>
2019-10-26 10:30:08 +0900

    aa
    aaa

---
+++ LIFULL_AR/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  LIFULL_AR
+//
+//  Created by 岡田龍太朗 on 2019/10/18.
+//  Copyright © 2019 岡田龍太朗. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        sceneView.scene = SCNScene()
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        sceneView.autoenablesDefaultLighting = true
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //最初にタップした座標を取り出す
+        guard let touch = touches.first else {return}
+        //スクリーン座標に変換する
+        let touchPos = touch.location(in: sceneView)
+        //タップされた位置にARアンカーを探す
+        let hitTest = sceneView.hitTest(touchPos, types: .existingPlaneUsingExtent)
+            if !hitTest.isEmpty {
+            //タップした箇所が取得できていればアンカーを追加
+                let anchor = ARAnchor(transform: hitTest.first!.worldTransform)
+                sceneView.session.add(anchor: anchor)
+        }
+
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        //玉のノードを作成
+        let sphereNode = SCNNode()
+        sphereNode.geometry = SCNSphere(radius: 0.05)
+        sphereNode.position.y += Float(0.05)
+        node.addChildNode(sphereNode)
+    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        // Create a session configuration
+//        let configuration = ARWorldTrackingConfiguration()
+//
+//        // Run the view's session
+//        sceneView.session.run(configuration)
+//    }
+    
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        
+//        // Pause the view's session
+//        sceneView.session.pause()
+//    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
